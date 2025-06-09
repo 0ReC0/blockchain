a7af50ea1ffc477d4deb0c2483a8def18f282eaa
s.kitanin <s.kitanin@mail.ru>
2025-06-10 00:14:18 +0300

    feat: Реализация BFT-консенсуса и улучшения в блокчейне
    - Переключение на BFT-консенсус через `ConsensusSwitcher`
    - Добавлена инициализация валидаторов и `ValidatorPool`
    - Реализована инициализация `BFTNode` с signer'ом на основе ECDSA
    - Перенос запуска P2P-сети и REST API в конец `main`
    - Исправлено расположение инициализации `double_spend` в конец
    - Улучшена структура кода с добавлением комментариев на русском

--- main.go
+++ main.go
@@ -42,42 +42,54 @@ func main() {
 	// ============ Инициализация блокчейна и пула транзакций ============
 	chain := blockchain.NewBlockchain()
 	txPool := txpool.NewTransactionPool()
+
+	// ============ Инициализация тестовой транзакции ============
 	tx1 := txpool.NewTransaction("A", "B", 10)
 	txPool.AddTransaction(tx1)
 
-	// ============ Инициализация сетевого уровня ============
-	go p2p.StartNetwork()
+	// ============ Инициализация валидаторов ============
+	validators := []*pos.Validator{
+		pos.NewValidator("validator1", 2000),
+		pos.NewValidator("validator2", 1000),
+	}
+	validatorPool := pos.NewValidatorPool(validators)
 
-	// ============ Инициализация API ============
-	apiServer := api.NewAPIServer(chain, txPool)
-	go apiServer.Start(":8081")
-
-	// ============ Инициализация консенсуса ============
-	// Используем только ConsensusSwitcher, без ConsensusManager
-	switcher := manager.NewConsensusSwitcher(manager.ConsensusPoS)
-
-	// Запуск консенсуса по таймеру
-	go func() {
-		ticker := time.NewTicker(10 * time.Second)
-		for {
-			select {
-			case <-ticker.C:
-				switcher.StartConsensus()
-			}
-		}
-	}()
+	// ============ Инициализация signer'а ============
+	signer, err := signature.NewECDSASigner()
+	if err != nil {
+		panic("Failed to create signer: " + err.Error())
+	}
 
 	// ============ Инициализация BFT-ноды ============
 	bftNode := bft.NewBFTNode(
 		"validator1",
-		pos.NewValidator("validator1", 2000),
-		pos.NewValidatorPool([]*pos.Validator{
-			pos.NewValidator("validator1", 2000),
-		}),
+		validators[0],
+		*validatorPool,
 		txPool,
 		chain,
-		&signature.ECDSASigner{}, // или другой signer
+		signer,
 	)
+	// ============ Инициализация ConsensusSwitcher ============
+	switcher := manager.NewConsensusSwitcher(manager.ConsensusBFT)
+
+	// ============ Запуск консенсуса через ConsensusSwitcher ============
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		for {
+			<-ticker.C
+			switcher.StartConsensus()
+		}
+	}()
+
+	// ============ Запуск P2P сети ============
+	go p2p.StartNetwork()
+
+	// ============ Запуск REST API ============
+	apiServer := api.NewAPIServer(chain, txPool)
+	go apiServer.Start(":8081")
+
+	// ============ Запуск защиты от двойных трат ============
+	double_spend.InitSecurity()
 
 	// Создаём Optimistic Rollup
 	optimisticRollup := rollup.NewOptimisticRollup(chain, bftNode)
@@ -91,9 +103,6 @@ func main() {
 	// ============ Инициализация шардов ============
 	shardMgr := sharding.NewShardManager()
 	shardMgr.CreateShard("0")
-
-	// ============ Инициализация безопасности ============
-	double_spend.InitSecurity()
 
 	// ============ Инициализация масштабируемости ============
 	executor := parallel.NewParallelExecutor(4)
