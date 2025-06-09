a115638d74433fb805b4b38b3eefba40b5078c18
s.kitanin <s.kitanin@mail.ru>
2025-06-09 22:50:34 +0300

    refactor: Удаление и реорганизация модулей
    - Удалены файлы:
      - `consensus/governance/reputation.go`
      - `consensus/manager/upgrade.go`
      - `network/rpc/service.go`
      - `privacy/private_tx/encryptor.go`
      - `privacy/private_tx/private_tx.go`
      - `privacy/shielded/shielded_block.go`
      - `privacy/shielded/shielded_pool.go`
    
    - Обновлён `main.go`:
      - Убраны неиспользуемые импорты.
      - Упрощена инициализация модулей.
      - Заменён `rpc.StartRPCServer` на `api.StartRPCServer`.
      - Обновлена логика инициализации говернанса.
      - Убраны закомментированные и дублирующиеся части кода.

--- main.go
+++ main.go
@@ -9,9 +9,7 @@ import (
 	"./consensus/bft"
 	"./consensus/manager"
 	"./consensus/pos"
-	"./crypto/encryption"
 	"./network/p2p"
-	"./network/rpc"
 
 	// Уровень хранения
 	"./storage/blockchain"
@@ -20,18 +18,11 @@ import (
 
 	// Модули безопасности
 	"./security/double_spend"
-	"./security/fiftyone"
-	"./security/sybil"
-
-	// Модуль приватности
-	"./privacy/private_tx"
-	"./privacy/shielded"
 
 	// Модули масштабируемости
 	"./scalability/parallel"
 
 	// Смарт-контракты
-	"./contracts/execution"
 
 	// Интеграция
 	"./integration/api"
@@ -40,59 +31,13 @@ import (
 
 	// Говернанс
 	"./governance/reputation"
-	"./governance/upgrade"
 	"./governance/voting"
 )
-
-func initSecurityModules() {
-	// Инициализация защиты от двойных расходов
-	doubleSpendGuard := double_spend.NewDoubleSpendGuard()
-	doubleSpendGuard.StartCleanup(5 * time.Minute)
-
-	// Инициализация защиты от Sybil-атак
-	validators := []string{"validator1", "validator2"}
-	sybilGuard := sybil.NewSybilGuard(validators)
-
-	// Инициализация защиты от атак 51%
-	validatorsPower := map[string]int64{
-		"validator1": 100,
-		"validator2": 100,
-		"validator3": 100,
-	}
-	attackGuard := fiftyone.NewFiftyOnePercentGuard(validatorsPower)
-	attackGuard.Monitor(10 * time.Second)
-}
-
-func initPrivacyModules() {
-	// Инициализация шифрования
-	aesEncryptor := &encryption.AESEncryptor{}
-	key := private_tx.GenerateKey("my-secret-password")
-
-	// Создание приватной транзакции
-	tx, _ := private_tx.NewPrivateTransaction("A", "B", 10, aesEncryptor, key)
-
-	// Расшифровка
-	decrypted, _ := tx.Decrypt(aesEncryptor, key)
-	fmt.Println("Decrypted:", string(decrypted))
-
-	// Пул приватных транзакций
-	pool := shielded.NewShieldedPool()
-	pool.AddTransaction(tx)
-
-	// Блок с приватными транзакциями
-	baseBlock := &blockchain.Block{
-		Index:    1,
-		PrevHash: "0",
-		Hash:     "1",
-	}
-	shieldedBlock := shielded.NewShieldedBlock(baseBlock, pool.GetTransactions(1))
-	fmt.Printf("Shielded block: %+v\n", shieldedBlock)
-}
 
 func main() {
 	// Инициализация сетевого уровня
 	go p2p.StartNetwork()
-	go rpc.StartRPCServer(":8080")
+	go api.StartRPCServer(":8080")
 
 	// Инициализация консенсуса
 	posManager := manager.NewConsensusManager(manager.ConsensusPoS)
@@ -109,12 +54,10 @@ func main() {
 	)
 
 	// Инициализация говернанса
-	upgradeMgr := manager.NewUpgradeManager()
-	upgradeMgr.SubmitUpgradeProposal(
-		"Update consensus protocol",
-		"Switch to faster BFT",
-		"validator1",
-	)
+	upgradeMgr := governance.NewUpgradeManager("v1.0.0")
+	upgradeMgr.ProposeUpgrade("v2.0.0", "Switch to BFT", time.Now().Add(24*time.Hour))
+	upgradeMgr.ApproveUpgrade()
+	_ = upgradeMgr.ApplyUpgrade()
 
 	// Инициализация блокчейна
 	chain := blockchain.NewBlockchain()
@@ -122,50 +65,18 @@ func main() {
 	// Инициализация пула транзакций
 	txPool := txpool.NewTransactionPool()
 	tx1 := txpool.NewTransaction("A", "B", 10)
-	tx2 := txpool.NewTransaction("B", "C", 5)
 	txPool.AddTransaction(tx1)
-	txPool.AddTransaction(tx2)
 
 	// Инициализация шардов
 	shardMgr := sharding.NewShardManager()
 	shardMgr.CreateShard("0")
-	shardMgr.CreateShard("1")
-	shardMgr.CreateShard("2")
 
 	// Инициализация безопасности
-	doubleSpendGuard := double_spend.NewDoubleSpendGuard()
-	doubleSpendGuard.StartCleanup(5 * time.Minute)
-
-	validators := []string{"validator1", "validator2"}
-	sybilGuard := sybil.NewSybilGuard(validators)
-
-	validatorsPower := map[string]int64{
-		"validator1": 100,
-		"validator2": 100,
-		"validator3": 100,
-	}
-	attackGuard := fiftyone.NewFiftyOnePercentGuard(validatorsPower)
-	go attackGuard.Monitor(10 * time.Second)
-
-	// Инициализация приватности
-	aesEncryptor := &encryption.AESEncryptor{}
-	key := private_tx.GenerateKey("my-secret-password")
-
-	tx, _ := private_tx.NewPrivateTransaction("A", "B", 10, aesEncryptor, key)
-	decrypted, _ := tx.Decrypt(aesEncryptor, key)
-	fmt.Println("Decrypted:", string(decrypted))
-
-	pool := shielded.NewShieldedPool()
-	pool.AddTransaction(tx)
+	double_spend.InitSecurity()
 
 	// Инициализация масштабируемости
 	executor := parallel.NewParallelExecutor(4)
 	executor.ExecuteTransactions(txPool.GetTransactions(100), chain)
-
-	// Инициализация смарт-контрактов
-	handler := execution.NewContractHandler()
-	tokenAddr := handler.DeployERC20("MyToken", "MTK", 18, 1_000_000)
-	fmt.Println("Token deployed at:", tokenAddr)
 
 	// Инициализация API
 	apiServer := api.NewAPIServer(chain, txPool)
@@ -175,18 +86,13 @@ func main() {
 	chainA := blockchain.NewBlockchain()
 	chainB := blockchain.NewBlockchain()
 	bridge := crosschain.NewChainBridge(chainA, chainB)
-	orcl := crosschain.NewCrossChainOracle()
-	orcl.Bridges = append(orcl.Bridges, bridge)
-	go orcl.MonitorChains()
 
 	// Инициализация банковского шлюза
 	bank := bank.NewBankGateway("api-key", "https://bank-api.com")
-	adapter := bank.NewBankAdapter(bank)
 
 	// Инициализация голосования
 	voting := voting.NewVotingModule()
 	reputation := reputation.NewReputationModule()
-	upgrade := upgrade.NewUpgradeManager("v1.0.0")
 
 	// Запуск всех компонентов
 	go posManager.Run()
